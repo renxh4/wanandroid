4e796015c1c053d6e974ec3f84c016b7f1a01042
renxiaohui <ren.xiaohui@immomo.com>
2022-09-21 10:27:47 +0800

    添加闪屏activity

---
+++ app/src/main/java/com/example/wanandroid/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wanandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+        finish()
+    }
+}
