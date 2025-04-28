a0f67ccfb7faf459e1ac48be92b9477a7281947e
renxh <18039395421@163.com>
2025-04-28 17:20:43 +0800

    添加测试activity

--- app/src/main/java/com/example/wanandroid/activity/CoroutineActivity.kt
+++ app/src/main/java/com/example/wanandroid/activity/CoroutineActivity.kt
@@ -3,10 +3,14 @@ package com.example.wanandroid.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wanandroid.R
+import com.example.wanandroid.databinding.ActivityCoroutineBinding
+import com.example.wanandroid.databinding.ActivityVideoBinding
 
 class CoroutineActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCoroutineBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video)
+        binding = ActivityCoroutineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/wanandroid/activity/TestBean.kt
+++ app/src/main/java/com/example/wanandroid/activity/TestBean.kt
@@ -1,3 +1,6 @@
 package com.example.wanandroid.activity
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
 data class TestBean(var name : String)
